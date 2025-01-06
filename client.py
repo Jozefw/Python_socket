81430972064d70f5b27116e4cb7e375420f34b51
Jozef <Jozefw@sbcglobal.net>
2025-01-06 14:28:21 -0800

    add infinite chat from server to client

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 import socket
 
 #create socket instance for client
-
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 host = '127.0.0.1'
 port = 8008
 
 #connect to socket server
-
 socket_client.connect((host,port))
 
-#client receives data from socket (in bytes)
-
-server_data = socket_client.recv(1024).decode()
-
-print(f'Here is the data:{server_data}')
-socket_client.close()
+while True:
+    msg = socket_client.recv(1024).decode()
+    if not msg:
+        break
+    print(msg)
+    data = input('Here is my response: ')
+    socket_client.sendall(str.encode(data))
+    
+socket_client.close();
--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import socket
-    
-    #create socket instance
-    
+    #create socket instance    
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
 host = '127.0.0.1'
 port = 8008
 
 #bind socket to host/port
-
 socket_server.bind((host, port))
 
 #listen for client 
-
 socket_server.listen()
 
 #waiting
-
-
 #accept ...this is done when the client is reaching out
-
 client, addr = socket_server.accept()
 
-print(f"Socket open:{addr} and connection {client}")
-
 #Server sends data to client FIRST
-
-
-server_data = "Here is your server packet"
-client.sendall(str.encode(server_data))
+while True:
+    data = input('Lets chat: ')
+    client.sendall(str.encode(data))
+    msg = client.recv(1024).decode()
+    if not msg:
+        break
+    print(msg)
 client.close()
